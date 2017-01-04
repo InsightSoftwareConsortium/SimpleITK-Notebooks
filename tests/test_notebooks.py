d76ddc830b10877e6bba0799a29c8375e53c796e
Ziv Yaniv <zivyaniv@nih.gov>
2017-01-04 12:57:26 -0500

    Resolved merge conflict.

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -374,3 +374,7 @@ class Test_notebooks(object):
     @pytest.mark.r_notebook
     def test_65_Registration_FFD_r(self):
         self.evaluate_notebook(self.absolute_path_r('65_Registration_FFD.ipynb'), 'ir')
+
+    @pytest.mark.r_notebook
+    def test_66_Registration_Demons_r(self):
+        self.evaluate_notebook(self.absolute_path_r('66_Registration_Demons.ipynb'), 'ir')
