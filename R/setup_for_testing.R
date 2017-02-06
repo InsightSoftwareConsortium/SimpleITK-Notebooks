1482bc84e1e46d269ec26ea865b63f2e0a7e0cd1
Ziv Yaniv <zivyaniv@nih.gov>
2017-02-06 11:25:42 -0500

    Updated the setup for testing in R, following the changes in Python.
    Using a decorator to replace the ReadImage function with a function
    that first reads the image and then resamples it to a smaller
    size. This is used for testing purposes in memory constrained
    environments.

--- R/setup_for_testing.R
+++ R/setup_for_testing.R
@@ -1,10 +1,18 @@
 library(SimpleITK)
 
+shrink_decorator <- function(size) {
+    inner_decorator <- function(func) {
+        func_and_resize <- function(...) {
+            shrink_filter <- ShrinkImageFilter()
+            shrink_filter$SetShrinkFactor(size)
+            return(shrink_filter$Execute(func(...)))
+        }
+        return(func_and_resize)
+    }
+    return(inner_decorator)
+}
+
 if("" != Sys.getenv("SIMPLE_ITK_MEMORY_CONSTRAINED_ENVIRONMENT")) { 
-  ReadImage <- function(...) {
-    shrink_filter <- ShrinkImageFilter()
-    shrink_filter$SetShrinkFactor(2)
-    return(shrink_filter$Execute(SimpleITK::ReadImage(...)))
-  }
+  ReadImage <- shrink_decorator(4)(SimpleITK::ReadImage)
 }
 
