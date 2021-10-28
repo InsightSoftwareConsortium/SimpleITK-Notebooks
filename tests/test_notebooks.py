1f9e3cd01d7be04d883535b60d05cf87539274ee
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2021-10-28 15:08:36 +0200

    renamed notebook in test

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -87,7 +87,7 @@ class Test_notebooks(object):
                               '03_Image_Details.ipynb',
                               '04_Image_Display.ipynb',
                               '05_Results_Visualization.ipynb',
-                              '10_matplotlib\'s_imshow.ipynb',
+                              '10_matplotlibs_imshow.ipynb',
                               '20_Expand_With_Interpolators.ipynb',
                               '21_Transforms_and_Resampling.ipynb',
                               '22_Transforms.ipynb',
